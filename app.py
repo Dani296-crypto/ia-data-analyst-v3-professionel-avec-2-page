c3d82ceee0d6a8481b0b8556c13d8263f1dca9d4
Dani296-crypto <abalod296@gmail.com>
2026-05-19 11:56:42 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Login IA", layout="centered")
+
+st.title("🔐 Connexion")
+
+password = st.text_input("Mot de passe", type="password")
+
+if st.button("Se connecter"):
+
+    if password == st.secrets["APP_PASSWORD"]:
+        st.session_state["auth"] = True
+        st.success("Accès autorisé")
+        st.switch_page("ia")
+
+    else:
+        st.error("Mot de passe incorrect")
