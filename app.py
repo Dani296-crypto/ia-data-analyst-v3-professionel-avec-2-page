2ab91e40c47cbec8781a0cea418e4900a26dd81a
Dani296-crypto <abalod296@gmail.com>
2026-05-19 12:22:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title="Login")
 
+st.title("🔐 Login")
+
 if "auth" not in st.session_state:
     st.session_state["auth"] = False
-
-st.title("🔐 Login")
 
 password = st.text_input("Mot de passe", type="password")
 
@@ -14,7 +14,10 @@ if st.button("Se connecter"):
     if password == st.secrets["APP_PASSWORD"]:
         st.session_state["auth"] = True
         st.success("Accès autorisé")
-        st.rerun()
 
     else:
         st.error("Mot de passe incorrect")
+
+# 👉 IMPORTANT : redirection logique
+if st.session_state["auth"]:
+    st.switch_page("pages/ia")
